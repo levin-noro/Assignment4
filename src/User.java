cd3c6eb804e937134ed8493e2e6721fb6d16b8ea
Levin <Levin@172.17.207.181>
2015-11-06 15:51:33 -0500

    Update

--- src/User.java
+++ src/User.java
@@ -8,48 +8,38 @@ import java.util.Scanner;
 public class User {
 	private String username;
 	// create a class constructor that calls username
-	User (String name) {username = name;} //
+	//User (String name) {username = name;} //
 	
 	public String getUsername(String name, int option) throws IOException { // stores the username.
 	// usernames of all registered users are stored in the file Users.txt
 		
 		File inFile = new File("Users.txt");
 		
-		if (option == 1) {
+		if (option == 1) 
+		{
 			
-			if (searchFile(inFile, name)) {
+			if (searchFile(inFile, name)) 
+			{
 				return "Hello Mr." + name;
 			}
-			else {
+			else 
+			{
 				return "No Access";
 			}
 
 			
 		}
-		else if (option == 2) {
-			// add case where user name already exists
-			//pWriter.println(name);
+		
+		else if (option == 2) 
+		{
 			String message = writeFile (inFile, name);
-//			pWriter.close();
-//			fWriter.close();
-//			sc.close();
 			return message;
+
 		}
 		
-		else
-		{	
+		else {	
 			return "Invalid Option";
 		}	
-		// Sign-up	
-			// return username successfully added
-			
-		
-			
-				
-			// else
-				// return access
-		
-		 
 		 
 	}
 	
@@ -62,26 +52,27 @@ public class User {
 		{
 			String line = sc.nextLine();
 			
-			if (line.equals(name)) // if user exists
-					{
-										    
+			if (line.equals(name))  // if user exists
+			{		
 					    pWriter.close();
 					    fWriter.close();
 					    sc.close();
 					    return true; // return Hello <user>
-						
-					}
+			}
 		
 		}
+		
 		pWriter.close();
 		fWriter.close();
 		sc.close();
 		return false;
+		
 	}
 	
 	private String writeFile (File inFile, String name ) throws IOException {
 		
-		if (searchFile(inFile, name)) {
+		if (searchFile(inFile, name)) 
+		{
 			return "Username already exists";
 		}
 		
@@ -89,12 +80,13 @@ public class User {
 		FileWriter fWriter = new FileWriter (inFile,true);
 		PrintWriter pWriter = new PrintWriter (fWriter);
 		
-		
+		// Appends name to inFile
 		pWriter.println(name);
 		
 		pWriter.close();
 		fWriter.close();
 		sc.close();
+		
 		return "Username successfully added";
 	}
 }
