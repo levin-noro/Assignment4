08c0223b4f8eeb975afb65ffc485d9098d125790
AlenaKa <magdalena.karski@gmail.com>
2015-11-06 14:35:24 -0500

    audio

--- src/CD.java
+++ src/CD.java
@@ -1,7 +1,7 @@
 
-public class CD {
+public class CD extends Audio {
 	@Override
-	public int getPrice(...) { // override to get the item price and add 2% (Environment Tax)
+	public int getPrice(String item) { // override to get the item price and add 2% (Environment Tax)
 	}
 	// // all info stored in CDs.txt: S.No, name of CD, artist, artist, price, quantity in Store, and type
 }
