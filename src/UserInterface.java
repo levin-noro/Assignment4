57381cafe10172db33ede69789961efe42e2cb8a
AlenaKa <magdalena.karski@gmail.com>
2015-11-06 10:35:41 -0500

    update

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
@@ -11,13 +12,13 @@ public class UserInterface{
 			return currentPage;
 		}
 		
-		public int changeCurrentPage(int currentPage) {// This method is for page navigation. It should change to current page and show the content.
+		public int changeCurrentPage() {// This method is for page navigation. It should change to current page and show the content.
 			Scanner scanner = new Scanner(System.in); 
 			switch (currentPage){
 		        
 				case 1:
 		            System.out.println("1.Sign in	P1");
-		            System.out.println("2.Sign up");
+		            System.out.println("2.Sign up"); System.out.println();
 		            System.out.print("Choose your option: ");
 		        	int page = Integer.parseInt(scanner.next());
 		        	if (page == 1) {currentPage(2);}
@@ -26,40 +27,115 @@ public class UserInterface{
 		        	break;
 		            
 		        case 2:
-		        	System.out.println("Choose your username:");
-		        	User newUser = new User(scanner.next());
-		        	getUsername(newUser.username);
-		        	System.out.println("Username successfully added");
+		        	System.out.println("Choose your username:"); System.out.println();
+		        	String newname = scanner.next()
+		        	User newUser = new User(newname);
+		        	User.getUsername(newname); // change
+		        	System.out.println("Username successfully added		P2");
 		        	scanner.close();
 		            break;
 		        
 		        case 3:
-		        	System.out.println("Enter your username:");
-		        	String name = scanner.next());
-		        	// include code to check if username is in the file
-		        	InputStream is = //--open an input stream from file--
-		        			 BufferedReader rd = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+		        	System.out.println("Enter your username:"); System.out.println();
+		        	String name = scanner.next();
+		        	scanner.close();
+		        	int found = 0;
+		        	InputStream is = new FileInputStream("Users.txt");
+		        			BufferedReader rd = new BufferedReader(new InputStreamReader(is,"UTF-8"));
 		        			 String line;
 
 		        			 while ( (line = rd.readLine()) != null ){
 		        				 // should we ignore case for usernames?
-		        			     if(line.matches(".*TestString.*")){ //--regex of what to search--
-		        			         //--do something---
-		        			    	 System.out.println("Username successfully added		P2");
-		        			    	 break; //--if not want to search further--
+		        			     if(line.matches(name)){
+		        			    	 System.out.println("Hello Mr."+name+"		P3");
+		        			    	 found = 1;
+		        			    	 break; 
 		        			     }
 		        			 }
 		        			 
+		        	if (found == 0) {
+		        		currentPage(4);
+		        		changeCurrentPage();
+		        	}
 		        	
-		        	scanner.close();
+		        	currentPage(5);
+	        		changeCurrentPage();
+		        	
+	        		// structure specified in assignment says that pages 3 and 4 are supposed to be separate but it seems like it would make sense to merge them
 		            break;
+		            
+		        case 4:
+		        	System.out.println("No Access");
+		        	currentPage(1);
+	        		changeCurrentPage();
+	        		break;
+		        	
+		        case 5:
+		        	
+		        	System.out.println("1.View Items By Category");
+		        	System.out.println("2.View Shopping Cart");
+		        	System.out.println("3.Sign Out"); System.out.println();
+		        	
+		        	System.out.println("Choose your option:");
+		        	System.out.println("							P5");
+		        	
+		        	switch(Integer.parseInt(scanner.next())) {
+		        	
+		        	case 1:
+		        		currentPage(6);
+		        		changeCurrentPage();
+		        		break;
+		        	
+		        	case 2:
+		        		currentPage(7);
+		        		changeCurrentPage();
+		        		break;
+		        		
+		        	case 3:
+		        		currentPage(1);
+		        		changeCurrentPage();
+		        		break;		        		
+		        	}
+		        	scanner.close();
+		        	break;
+		        	
+		        case 6:
+		        case 7:
+		        	System.out.println("1.Readables");
+		        	System.out.println("2.Audio"); System.out.println();
+		        	System.out.println("Choose your option:"); System.out.println();
+		        	System.out.println("Press -1 to return to the previous menu");
+		        	
+		        /*	switch(Integer.parseInt(scanner.next())) {
+		        	
+		        	case 1:
+		        		currentPage(6);
+		        		changeCurrentPage();
+		        		break;
+		        	
+		        	case 2:
+		        		currentPage(7);
+		        		changeCurrentPage();
+		        		break;
+		        		
+		        	case 3:
+		        		currentPage(1);
+		        		changeCurrentPage();
+		        		break;		        		
+		        	}
+		        	scanner.close();
+		        	break;
+		        	*/
+		        	
+		        case 8:
+		        
 		            
 		        default:
 		            System.out.println("Default case");
 		            break;
 		    
 		    }
-			return changeCurrentPage(currentPage);              
+			return changeCurrentPage();              
 			   
 			
 		}
