2088b0c710e2ada2d9bd8d5c9639ebe1d4495d01
Levin <Levin@Levin>
2015-11-08 17:10:15 -0500

    Update

--- src/User.java
+++ src/User.java
@@ -9,16 +9,19 @@ public class User {
 	private String username;
 	// create a class constructor that calls username
 	//User (String name) {username = name;} //
+	public User (String name) {
+		this.username = name;
+	}
 	
 	public String getUsername(String name, int option) throws IOException { // stores the username.
 	// usernames of all registered users are stored in the file Users.txt
 		
-		File inFile = new File("Users.txt");
+		//File inFile = new File("Users.txt");
 		
 		if (option == 1) 
 		{
 			
-			if (searchFile(inFile, name)) 
+			if (searchFile("Users.txt", name)) 
 			{
 				return "Hello Mr." + name;
 			}
@@ -32,7 +35,7 @@ public class User {
 		
 		else if (option == 2) 
 		{
-			String message = writeFile (inFile, name);
+			String message = writeFile ("Users.txt", name);
 			return message;
 
 		}
@@ -43,7 +46,8 @@ public class User {
 		 
 	}
 	
-	private boolean searchFile (File inFile, String name) throws IOException {
+	public boolean searchFile (String filename, String name) throws IOException {
+		File inFile = new File(filename);
 		Scanner sc = new Scanner (inFile);
 		FileWriter fWriter = new FileWriter (inFile,true);
 		PrintWriter pWriter = new PrintWriter (fWriter);
@@ -69,15 +73,16 @@ public class User {
 		
 	}
 	
-	private String writeFile (File inFile, String name ) throws IOException {
+	private String writeFile (String filename, String name ) throws IOException {
 		
-		if (searchFile(inFile, name)) 
+		if (searchFile(filename, name)) 
 		{
 			return "Username already exists";
 		}
 		
-		Scanner sc = new Scanner (inFile);
-		FileWriter fWriter = new FileWriter (inFile,true);
+		File infile = new File(filename);
+		Scanner sc = new Scanner (infile);
+		FileWriter fWriter = new FileWriter (infile,true);
 		PrintWriter pWriter = new PrintWriter (fWriter);
 		
 		// Appends name to inFile
