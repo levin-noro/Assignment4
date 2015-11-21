528644b69481767190b7aaff7b17d1206560b06b
AlenaKa <magdalena.karski@gmail.com>
2015-11-21 17:08:56 -0500

    finished working on this file - commenting

--- src/HWK4_karskim.java
+++ src/HWK4_karskim.java
@@ -6,12 +6,11 @@ import java.io.IOException;
 /* This class contains the main method. Which starts the UserInterface at page 1.*/
 public class HWK4_karskim {
 	
-	// Starts the UserInterface at 
-	public static void main(String args[]) throws IOException { 
+	// Starts the UserInterface at page 1
+	public static void main(String args[]) throws IOException { // The methods called through UserInterface access files. The IO exception handles cases where these files cannot be accessed. 
 				
-		UserInterface Inter = new UserInterface(1);
-		Inter.currentPage(1);
-		Inter.changeCurrentPage();
+		UserInterface Inter = new UserInterface(1); // Initializes a new UserInterface object and sets currentPage to 1
+		Inter.changeCurrentPage(); // Navigates to the currentPage
 				
 	}
 }
