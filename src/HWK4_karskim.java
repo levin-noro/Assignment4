26e395c83009086e37abdafc0a11d8a4fbe399d1
AlenaKa <magdalena.karski@gmail.com>
2015-11-21 17:05:41 -0500

    starting to work on this file - commenting

--- src/HWK4_karskim.java
+++ src/HWK4_karskim.java
@@ -3,8 +3,10 @@ import java.io.IOException;
 //We rewrote serial numbers to make them unique to each item as follow:
 // books start from 1; ebooks starts from 2; MP3 starts from 3; CDs starts from 4
 
+/* This class contains the main method. Which starts the UserInterface at page 1.*/
 public class HWK4_karskim {
 	
+	// Starts the UserInterface at 
 	public static void main(String args[]) throws IOException { 
 				
 		UserInterface Inter = new UserInterface(1);
