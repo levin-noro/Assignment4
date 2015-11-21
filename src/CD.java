c426edeb71aa986c2a087cd8b0b722fd1f335c1e
AlenaKa <magdalena.karski@gmail.com>
2015-11-21 17:00:40 -0500

    Finished working on this file - commenting

--- src/CD.java
+++ src/CD.java
@@ -1,9 +1,9 @@
 /* This class is an extension of the class Audio. It contains only one method, getPrice(), which overrides the parent method.*/
 
 public class CD extends Audio {
-	@Override
+	@Override // override getPrice() in Audio
 	public double getPrice() { // override to get the item price and add 2% (Environment Tax)
-		return 1.02*this.price;
+		return 1.02*this.price; // get the price of the CD object and add a 2% environment tax. Return the price.
 	}
-	// all info stored in CDs.txt: S.No, name of CD, artist, artist, price, quantity in Store, and type
+	
 }
