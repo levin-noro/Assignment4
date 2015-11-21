26117ed7a3da8a92a7fda15ef32c40db37ad316a
AlenaKa <magdalena.karski@gmail.com>
2015-11-21 17:10:34 -0500

    starting to work on this file - commenting

--- src/Item.java
+++ src/Item.java
@@ -1,14 +1,11 @@
 
 public abstract class Item{
-		
 	
-		
-		protected int sNo;
-		protected String title;
-		protected double price;
+		protected int sNo; // protected int sNo is the serial number of the item
+		protected String title; // the title of the item
+		protected double price; // the base price of the item
 		protected int quantity;
 		protected String itemtype;
-//		public abstract String[] getInfo(String filename, int lineNumber);
 		public abstract double getPrice();
 		
 }
