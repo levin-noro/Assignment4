84d44d4f87ab25d020110f2df1284afd4365a8ae
AlenaKa <magdalena.karski@gmail.com>
2015-11-21 16:54:49 -0500

    starting to work on this file - commenting

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+/* This class is an extension of the abstract class item. */
+
 public class Book extends Readable {
 		@Override  
 		public double getPrice() { // override to get the item price and add 2% (Environment Tax)
