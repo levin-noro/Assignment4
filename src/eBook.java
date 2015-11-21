e35fc2e2271bb2b72654a3ea91eb5a95174f4e0c
AlenaKa <magdalena.karski@gmail.com>
2015-11-21 17:03:18 -0500

    Finished working on this file - commenting

--- src/eBook.java
+++ src/eBook.java
@@ -1,9 +1,9 @@
 /* This class is an extension of the class Readables. It contains only one method, getPrice(), which overrides the parent method.*/
 
 public class eBook extends Readable{
-			@Override // 
-			public double getPrice() { // override and only call the parent's constructor to get the base price.
-				return this.price;
+			@Override // Override the method getPrice() in Readables 
+			public double getPrice() { // override to get the base price.
+				return this.price; // return the base price of the eBook object
 			}
-			// all info stored in EBooks.txt: S.No, name of ebook, author, artist, price, quantity in Store, and type
+			
 }
